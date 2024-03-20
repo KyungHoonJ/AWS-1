e37ace2659c8aac65fe1bc75e6169a090cff3d0c
KyungHoonJ <rudekrudek@naver.com>
2024-03-20 13:29:45 +0900

    feat: require, module

--- 9.NodeJS/240320/test2.js
+++ 9.NodeJS/240320/test2.js
@@ -3,5 +3,15 @@ console.log("test2.js 파일을 읽기 시작했어.");
 console.log(global.b);
 
 let text = "이건 ES6 문법이다.";
-export { text };
-export default { text2: text + 2 };
+
+module.exports = {
+  consoleTest() {
+    console.log(text);
+  },
+  consoleStr(str) {
+    console.log(str);
+  },
+  setText(_text) {
+    text = _text;
+  },
+};
--- 9.NodeJS/240320/test_global.js
+++ 9.NodeJS/240320/test_global.js
@@ -1,8 +1,5 @@
 const test1 = require("./test1.js");
-// const test2 = require("./test2.js");
-// import { text } from "./test2.js";
-// import test2 from "./test2.js";
-// 참조?
+const test2 = require("./test2.js");
 // 가져오기 import
 
 global.a = 1;
@@ -11,7 +8,8 @@ console.log(global.a);
 console.log(a);
 
 console.log(test1);
-// console.log(test2);
-// console.log(text);
-
+test2.consoleTest();
+test2.setText("아니야, 이건 바닐라 자바스크립트야");
+test2.consoleTest();
+test2.consoleStr("이것도 확인해볼까?");
 // export, 내보내기
