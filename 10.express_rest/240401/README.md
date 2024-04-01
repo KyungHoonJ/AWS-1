e2fc5459cc931f15a85672618ea4138564a4abc0
KyungHoonJ <rudekrudek@naver.com>
2024-04-01 11:36:38 +0900

    feat: express setting

---
+++ 10.express_rest/240401/README.md
@@ -0,0 +1,16 @@
+# express server setting
+
+```bash
+npm init
+npm i express morgan dotenv
+npm i -D nodemon
+```
+
+## dotenv
+
+- Environment
+- 환경변수
+- 환경설정에 있는 변수 << 환경이라는 뭘까?
+- 개발 환경에서 작업 중에 있다.
+- 서비스 환경
+- port, 개발 환경 : 3000 | 배포 환경 : 80
---
+++ 10.express_rest/240401/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
+
+dotenv.config();
+
+// console.log(process.env["PORT"]);
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+// global.port = process.env.PORT;
+
+app.use(morgan("dev"));
+// app.use((req, res, next) => {
+//   console.log(req.hostname);
+//   next();
+// });
+
+app.use(express.urlencoded({ extended: false }));
+// body parser
+// 메서드 호출할 때 객체가 들어간다? << 옵션
+// extended : 확장
+//   - true | false
+//   - true : 외부 라이브러리를 사용하여 작동한다. (qs library)
+//   - false : Express가 갖고있는 body parser로 작동한다. (querystring module)
+// querystring을 파싱해준다? << form => Content-Type : x-www-form-urlencoded
+
+app.use(express.json());
+// Content-Type : application/json
+
+app.use("/", express.static("public"));
+// / == public
+// /imgs => public/imgs
+// /imgs/wa4v34va.png => public/imgs/wa4v34va.png => X
+// app.use(express.static("public"));
+app.use("/imgs", express.static("uploads"), express.static("uploads1"));
+// /imgs == uploads
+// /imgs/wa4v34va.png => uploads/wa4v34va.png => O
+
+// /imgs/wa4v34va2.png => uploads/wa4v34va2.png => X => 아래 코드 실행
+
+app.get("/board", (req, res) => {
+  console.log("board 폴더는 없다");
+  //   console.log(req.hostname);
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.put("/", (req, res) => {
+  res.json({ a: 1 });
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port") + " 포트로 서버를 열었어");
+});
