5cbeea94923ecb40894d60cbe4f5cba21204c7d3
KyungHoonJ <rudekrudek@naver.com>
2024-04-25 12:21:15 +0900

    feat: add user control

--- 12.Sequelize/240425/router/api/todo.js
+++ 12.Sequelize/240425/router/api/todo.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const {Todo} = require('../../models')
+
+router.post('/add', async (req, res)=>{
+    if(req.body.todoId == "")req.body.todoId = undefined
+    await Todo.create({...req.body, userId: req.cookies.userId})
+    res.redirect('/')
+})
 
 module.exports = router;
--- 12.Sequelize/240425/router/view/index.js
+++ 12.Sequelize/240425/router/view/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {Todo} = require('../../models')
+const {Todo, sequelize, Sequelize} = require('../../models')
 
 router.use((req, res, next)=>{
   res.data = { user: null, regist: false, list: [] };
@@ -10,7 +10,24 @@ router.get("/", async (req, res) => {
   res.data.user = req.cookies.user;
 
   if(req.cookies.userId){
-    res.data.list = await Todo.findAll();
+    res.data.list = await Todo.findAll({
+      attributes:['content', 'id',
+        [sequelize.fn('count', Sequelize.col('Todos.id')), 'todoCnt']
+      ],
+      where:{
+        todoId: null
+      },
+      include:[{
+        model: Todo,
+        attributes:[],
+      }],
+
+      group:[Sequelize.col('Todo.id')]
+    });
+    if(res.data.list.length){
+      console.log(JSON.parse(JSON.stringify(res.data.list[0])))
+      console.log(res.data.list[0].todoCnt)
+    }
   }
 
   res.render("index", res.data);
