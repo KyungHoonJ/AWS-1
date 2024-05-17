c9e3f0607368b7a59ae414aef28570f61570f646
KyungHoonJ <rudekrudek@naver.com>
2024-05-17 10:20:03 +0900

    feat: adding rooms

--- 14.web-socket/240516/server.js
+++ 14.web-socket/240516/server.js
@@ -6,12 +6,58 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: { origin: ["http://127.0.0.1:5501", "http://127.0.0.1:8080"] },
-});
+}); // url의 host
 // You are trying to attach socket.io to an express request handler function. Please pass a http.Server instance.
+// path(router) => socket : namespace
+// queryString || cookie || variable(React) => socket : room
 
 app.use(express.static("public"));
 // io.use(cors());
 
+const chat = io.of("chat");
+// app.use('/chat', chat);
+chat.on("connection", (client) => {
+  console.log("connected chat");
+  // chat.js 파일 내에서 get, post
+  client.on("disconnect", () => {
+    console.log("client disconnect");
+  });
+
+  console.log(client.rooms);
+  // Set(1) { 'TllJgx-jMjkGUK4RAAAD' }
+  // const temp = new Set();
+  // console.log(temp);
+  // temp.add(2);
+  // console.log(temp);
+  // temp.add(1);
+  // console.log(temp);
+  // temp.add("1");
+  // console.log(temp);
+  // console.log(new Set([1, 2, 3, 1, 2, 3, 1, 2, 3]));
+  // const tempObj = { a: 1 };
+  // console.log(
+  //   new Set([tempObj, tempObj, tempObj, tempObj, tempObj, tempObj, tempObj])
+  // );
+  // client.rooms.clear();
+  // client.join(1);
+  // console.log(client.rooms);
+
+  client.on("chat", (data) => {
+    let now = [...client.rooms][0];
+    if (data.room !== now) {
+      client.rooms.clear();
+      client.join(data.room);
+      now = data.room;
+    }
+    console.log(data);
+    chat.to(now).emit("chat", data);
+    // chat.to(2).emit("chat", data);
+    // client.leave(1);
+    // iterator
+  });
+});
+
+// app.get('/', (req, res)=>{}) ==
 io.on("connection", (client) => {
   console.log("client connected");
 
