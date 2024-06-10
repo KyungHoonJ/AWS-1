1b3e6d6a655b34fdbe4d8f7f854e8f21cd289c8c
KyungHoonJ <rudekrudek@naver.com>
2024-06-10 12:07:28 +0900

    fix: edit append Element

--- 16.plan/240610/arcalive/index.js
+++ 16.plan/240610/arcalive/index.js
@@ -50,19 +50,229 @@ class Row {
   constructor({ list }) {
     this.element = document.createElement("div");
     list.forEach((item) => {
-      this.element.append(new Channel(item));
+      this.element.append(new Channel(item).element);
     });
   }
 }
 
-document
-  .getElementsByClassName("contents")[0]
-  .append(
-    new Row().element,
-    new Row().element,
-    new Row().element,
-    new Row().element
-  );
+document.getElementsByClassName("contents")[0].append(
+  new Row({
+    list: [
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+    ],
+  }).element,
+  new Row({
+    list: [
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+    ],
+  }).element,
+  new Row({
+    list: [
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+    ],
+  }).element,
+  new Row({
+    list: [
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+      {
+        title: "명조 채널",
+        list: [
+          {
+            title: "각 보스별 취얍속성같은거 있음?",
+            commentCount: 1,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "오늘의 보라왜가리 파밍은 망했다.",
+            commentCount: 0,
+            updatedAt: Date.now() - 4 * 60 * 1000,
+          },
+          {
+            title: "홀로그램 고릴라 5단계 1돌 음림 솔플",
+            commentCount: 2,
+            updatedAt: Date.now() - 5 * 60 * 1000,
+          },
+          {
+            title: "에코파밍 진짜 힘드네",
+            commentCount: 3,
+            updatedAt: Date.now() - 6 * 60 * 1000,
+          },
+        ],
+      },
+    ],
+  }).element
+);
 
 const channel = new Channel({
   title: "명조 채널",
